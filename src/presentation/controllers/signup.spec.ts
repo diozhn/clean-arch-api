312bb6b55cb77a8298395399f033edec4ec5fceb
Diogo Mendes <contato.diogomendes@gmail.com>
2022-05-18 17:59:52 -0300

    refactor: move all error imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,4 @@
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { SignUpController } from './signup'
 import { EmailValidator } from '../protocols/email-validator'
 
